19298d67f53915ceab1e77f71e63a2d39c11498b
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2024-03-30 22:40:57 +0200

    Fix techs

--- prototypes/assemblers.lua
+++ prototypes/assemblers.lua
@@ -2,7 +2,6 @@ require("utils.scale")
 
 for i=1,1 do
     local entity, recipe, item = create_big_version("assembling-machine-" .. tostring(i), "assembling-machine")
-    recipe.enabled = true
     entity.next_upgrade = nil
 
     data:extend{entity, recipe, item}
--- prototypes/furnace.lua
+++ prototypes/furnace.lua
@@ -2,9 +2,19 @@ require("utils.scale")
 
 local stoneFurnace, stoneFurnaceRecipe, stoneFurnaceItem = create_big_version("stone-furnace", "furnace")
 stoneFurnace.next_upgrade = "steel-furnace-big"
-stoneFurnaceRecipe.enabled = true
 local steelFurnace, steelFurnaceRecipe, steelFurnaceItem = create_big_version("steel-furnace", "furnace")
 local electricFurnace, electricFurnaceRecipe, electricFurnaceItem = create_big_version("electric-furnace", "furnace")
+
+table.insert(data.raw.technology["advanced-material-processing"].effects, {
+    type = "unlock-recipe",
+    recipe = "steel-furnace-big",
+})
+
+table.insert(data.raw.technology["advanced-material-processing-2"].effects, {
+    type = "unlock-recipe",
+    recipe = "electric-furnace-big",
+})
+
 
 data:extend{
     stoneFurnace, stoneFurnaceRecipe, stoneFurnaceItem,
