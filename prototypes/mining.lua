7ebe3ce4f1fa0651575b64580048487b6f3792e3
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2024-03-31 00:01:08 +0200

    Fix burner mining drill recipe

--- prototypes/mining.lua
+++ prototypes/mining.lua
@@ -16,9 +16,19 @@ end
 local burnerMiner = createBigMiner("burner-mining-drill", BURNER_MINER_PERFORMANCE, 2)
 burnerMiner.radius_visualisation_picture = table.deepcopy(data.raw["mining-drill"]["electric-mining-drill"].radius_visualisation_picture)
 
+local burnerMinerRecipe = table.deepcopy(data.raw["recipe"]["burner-mining-drill-big"])
+
+for _, ingredients in pairs({burnerMinerRecipe.normal.ingredients, burnerMinerRecipe.expensive.ingredients}) do
+    for i, ingredient in ipairs(ingredients) do
+        if ingredient[1] == "stone-furnace" then
+            ingredient[1] = "stone-furnace-big"
+        end
+    end
+end
+
 local electricMiner = createBigMiner("electric-mining-drill", BURNER_MINER_PERFORMANCE * 2, 5)
 
 data:extend{
-    burnerMiner,
+    burnerMiner, burnerMinerRecipe,
     electricMiner,
 }
